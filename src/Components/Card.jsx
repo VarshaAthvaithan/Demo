39f2ddd30cafd1d78d3be3c74f96f63bdc799079
VarshaAthvaithan <varshavidhya009@gmail.com>
2024-06-11 11:47:30 +0530

    first commit

---
+++ src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="input-group">
+  <span className="input-group-text">ENTER THE NOTES HERE</span>
+  <textarea className="form-control" aria-label="With textarea"></textarea>
+</div>
+  )
+}
+
+export default Card
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <h1 className='text-bg-primary p-3 text-center d-flex justify-content-center'>NOTES APP</h1>
+   
+  )
+}
+
+export default Navbar
